0f2278617ee3015b168b4902bf56264c2978f26b
mustafa <mserhat.uslu@bombay.io>
2024-07-10 11:17:30 +0300

    Improve architecture via Subject.tsx

--- src/JsLearn/ObjectsArrays/Objects.tsx
+++ src/JsLearn/ObjectsArrays/Objects.tsx
@@ -1,20 +1,6 @@
-import React from "react";
+import { Example } from "../Utils/types/types";
 
-import { Example } from "../Utils/types/types.ts";
-import { CodeBlock } from "../Utils/CodeBlock.tsx";
-
-export const Objects = () => {
-  return (
-    <div className="subject">
-      <h3 className="subject-title">Objects Trivia</h3>
-      {ObjectExamples.map((ex, i) => (
-        <CodeBlock key={i} desc={ex.desc} code={ex.code} />
-      ))}
-    </div>
-  );
-};
-
-const ObjectExamples: Example[] = [
+export const ObjectExamples: Example[] = [
   {
     desc: "Object with keys will only take in to account the last key & value of key.",
     code: "const o = { M: { a: 1 }, M: { a: 99 } }; return o.M;",
